1ea26292743a5ef5c3580cfed172bdfe123e65b9
Rodrigo Silva de Azevedo <128925998+cgfrs@users.noreply.github.com>
2023-04-19 22:44:05 -0300

    Add files via upload

---
+++ Dia1/index.js
@@ -0,0 +1,37 @@
+//Imprimindo em tel
+console.log('Java script funionando');
+
+
+//VÁRIÁVEIS JS
+let firstName = 'Rodrigo'; //variavel não pode começar com número
+let lastName = 'Azevedo';
+const years = '36'; //Usar a variável const para não ser possivel 
+
+
+//OBJETOS JS
+let client =  {
+    firstName: 'Rodrigo',
+    lastName: 'Azevedo',
+    years: 38
+}
+console.log(client)
+
+//ARRAYS
+let friends = ['Carol', 'Nando', 'Felipe']
+friends[2] = 'Carlos' // Substitui o arrey de número 2 que no caso é o Felipe
+console.log(friends)
+
+//FUNÇÕES - FUNCTIONS com tarefas
+function saleStatus(status, total) {
+   console.log('transaction '  + status + ' total amount: $ ' + total)
+}
+saleStatus('approved', 30)
+
+//FUNÇÕES - FUNCTIONS com cálculos matemkáticos
+
+
+
+
+
+
+
