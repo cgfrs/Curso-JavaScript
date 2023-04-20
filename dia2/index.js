190455db84e7b0c378373f1f267393cc464ea471
Rodrigo Silva de Azevedo <128925998+cgfrs@users.noreply.github.com>
2023-04-20 18:57:55 -0300

    Add files via upload

---
+++ dia2/index.js
@@ -0,0 +1,29 @@
+//OPERADORES DE ATRIBUIÇÃO
+
+//let num1 = 5
+//let nome = 'Rodrigo'
+
+//console.log(num1)
+//console.log(num1 += 20)
+
+// OPERADORES DE COMPARAÇÃO
+//console.log(num1 > 0)
+//console.log(num1 < 0)
+//console.log(num1 >= 0)
+//console.log(num1 <= 0)
+
+//console.log(num1 === 5) //PARA SABER SE É IGUAL
+//console.log(num1 !== 5) //PARA SABER SE NÃO É IGUAL
+
+//console.log(nome === 'Rodrigo') //PARA SABER SE É IGUAL
+//console.log(nome !== 'Rodrigo') //PARA SABER SE NÃO É IGUAL
+
+
+// OPERADORES DE ternário
+
+//let driver = 150
+//let speed = driver > 110 ? 'acima' : 'abaixo'
+//console.log(speed);
+
+    //OPERADORES LÓGICOS
+    
