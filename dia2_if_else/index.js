ccce479f7d870a802684e1c8d155f6d5b980a6a1
Rodrigo Silva de Azevedo <128925998+cgfrs@users.noreply.github.com>
2023-04-24 23:24:08 -0300

    Add files via upload

---
+++ dia2_if_else/index.js
@@ -0,0 +1,62 @@
+//OPERADORES DE ATRIBUIÇÃO
+
+//let num1 = 5
+//let nome = 'Rodrigo'
+
+//console.log(num1)
+//console.log(num1 += 20)
+
+// OPERADORES DE COMPARAÇÃO
+//console.log(num1 > 0)
+//console.log(num1 < 0)
+//console.log(num1 >= 0)
+//console.log(num1 <= 0)
+
+//console.log(num1 === 5) //PARA SABER SE É IGUAL
+//console.log(num1 !== 5) //PARA SABER SE NÃO É IGUAL
+
+//console.log(nome === 'Rodrigo') //PARA SABER SE É IGUAL
+//console.log(nome !== 'Rodrigo') //PARA SABER SE NÃO É IGUAL
+
+
+// OPERADORES DE ternário
+
+//let driver = 150
+//let speed = driver > 110 ? 'acima' : 'abaixo'
+//console.log(speed);
+
+    //IF and ELSE
+    let driverSpeed = 60
+
+    if (driverSpeed == 80) {
+        console.log('Fast')
+    } 
+    else if (driverSpeed < 40 && driverSpeed < 80) {
+        console.log('Slow')
+    }
+
+    else if (driverSpeed == 80) {
+        console.log('Normal')
+    }
+
+    else {
+        console.log('Very Fats')
+    }
+    
+
+
+
+    let nome = ('Rodrigo')
+
+    switch (nome) {
+        case Rodrigo:
+            console.log(Rod)
+            break;
+
+            case Eduardo:
+            console.log(Edu)
+            break;
+    
+        default:
+            break;
+    }
