7cbbdc2aa4408f31c5b176247d0f2e9f4d1cdc5f
Rodrigo Silva de Azevedo <128925998+cgfrs@users.noreply.github.com>
2023-04-28 15:16:47 -0300

    Add files via upload

--- dia7_PROG_ORIENT_OBJ/1_Criando_um_Objeto/4_Construtores.js
+++ dia7_PROG_ORIENT_OBJ/1_Criando_um_Objeto/4_Construtores.js
@@ -1,14 +1,19 @@
 
-function CreateCar(color, year) {
+function CreateCar(color, year, model) {
         this.carColor= color;
-        this.carYear= year 
+        this.carYear= year;
+        this.carModel= model;
+        
     }
     
     let car1 = new CreateCar('Red', '2023')
        
     console.log(car1)
 
-    let car2 = new CreateCar('Blue', '2022')
+    let car2 = new CreateCar('Blue', '2022', 'XXR')
 
     console.log(car2)
+
+    
+
     
