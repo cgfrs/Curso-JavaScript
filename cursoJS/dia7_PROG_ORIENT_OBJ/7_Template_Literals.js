4fb39d03b7e0342729e71155945d29e0cf98af4a
Rodrigo Silva de Azevedo <128925998+cgfrs@users.noreply.github.com>
2023-05-02 20:05:51 -0300

    Add files via upload

--- cursoJS/dia7_PROG_ORIENT_OBJ/7_Template_Literals.js
+++ cursoJS/dia7_PROG_ORIENT_OBJ/7_Template_Literals.js
@@ -6,8 +6,16 @@
 
 
     //incluinso uma variável no textto e usando a crase para quebrar a linha
-    
+
     let name = 'Rodrigo'
-const email= `Hey ${name}. 
-Lets play`
-    console.log(email)
+    let team = 'Gama'
+    let company = 'GbTech'
+
+    let email = `
+    My name is ${name}
+    I'm from the team ${team} and I have the honor 
+    of working for the company ${company}`
+        console.log(email)
+
+
+
